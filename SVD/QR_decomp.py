14d1dd1255c2dd02aed06252042b2ab56dc1cbb5
Ved J0shi <vedjoshi19104@gmail.com>
2024-11-03 00:08:25 -0400

    SVD stuff

---
+++ SVD/QR_decomp.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+def QR_decomp(A):
+    A = A.astype('f')
+    n, m = A.shape
+    Q = np.empty((n,n))
+    R = np.zeros((n, m))
+    for i in range(n):
+        u = A[:, i]
+        for j in range(i-1, -1, -1):
+            R[j, i] = np.dot(A[:, i], Q[:, j])
+            u = u - R[j, i]*Q[:, j]
+        Q[:, i] = u/np.linalg.norm(u)
+        R[i, i] = np.dot(A[:, i], Q[:, i] )
+    if m > n:
+        for i in range(n, m):
+            for j in range(n-1, -1, -1):
+                R[j, i] = np.dot(A[:, i], Q[:, j])
+    return Q, R
+       
+
+
+
---
+++ SVD/img_approx.py
@@ -0,0 +1,32 @@
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.image import imread
+
+
+def image_approximation(fname, rank=100, dir='../test_data'):
+    '''
+    plots a low rank compression of the original image;
+    returns % contribution of captured singular values and % data storage
+    '''
+
+    segments = dir.split('/')
+    rgb = imread(os.path.join(*segments, fname)) #returns an m*n*3 numpy array
+    greyscale = np.mean(rgb,-1)
+    #print(greyscale.shape)
+    U, s, V_ = np.linalg.svd(greyscale, full_matrices=False)
+    S = np.diag(s)
+    print(U.shape, S.shape, V_.shape)
+    Approx = U[:,:rank]@S[:rank,:rank]@V_[:rank,:]
+    original_storage = greyscale.shape[0] * greyscale.shape[1]
+    compressed_storage = U.shape[0]*rank + rank + rank*V_.shape[1]
+    img = plt.imshow(Approx)
+    plt.show()
+    return np.sum(s[:rank])/np.sum(s), compressed_storage/original_storage
+    
+
+
+
+
+
+    
